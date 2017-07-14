31d86a774acc5fc596922fd18cd1efb18c5f40e0
notyy <notyycn@gmail.com>
2017-07-14 15:08:06 +0800

    add a BDD style table driven sample

--- src/test/scala/sample/TransferServiceSpec.scala
+++ src/test/scala/sample/TransferServiceSpec.scala
@@ -1,8 +1,9 @@
 package sample
 
-import org.scalatest.{FunSpec, Matchers}
+import org.scalatest.prop.TableDrivenPropertyChecks
+import org.scalatest.{FunSpec, GivenWhenThen, Matchers}
 
-class TransferServiceSpec extends FunSpec with Matchers {
+class TransferServiceSpec extends FunSpec with Matchers with TableDrivenPropertyChecks with GivenWhenThen {
   describe("TransferService"){
     it("can transfer money from source account to target account"){
       val srcAccount = new Account("xx",100.0)
@@ -13,6 +14,27 @@ class TransferServiceSpec extends FunSpec with Matchers {
     }
     it("transfer should fail if not enough balance in source account"){
       pending
+    }
+    //this is a scalatest table-driven-property-check example
+    it("can transfer any amount of money"){
+      val example =
+        Table(
+          ("source account balance","target account balance","transfer amount"," expected source account balance","expected target account balance"),
+          (100, 50, 50, 50, 100),
+          (77, 0, 50, 27, 50)
+        )
+      forAll(example){ (srcBalance, targetBalance, amount, expSrcBalance,expTargetBalance) =>
+        Given(s"source account who's owner is xx,have balance: $srcBalance")
+        val srcAccount = new Account("xx", srcBalance)
+        And(s"target account who's owner is yy, have balance: $targetBalance")
+        val targetAccount = new Account("yy", targetBalance)
+        When(s"Transfer $amount from srcAccount to targetAccount")
+        TransferService.transfer(srcAccount, targetAccount, amount)
+        Then(s"srcAccount should have balance $expSrcBalance")
+        srcAccount.balance shouldBe expSrcBalance
+        And(s"targetAccount should have balance $expTargetBalance")
+        targetAccount.balance shouldBe expTargetBalance
+      }
     }
   }
 }
